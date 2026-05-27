90039f13307de373bdefbfab76871ccd55a31448
emorio00 <emorio00@estudiantes.unileon.es>
2026-05-27 21:29:59 +0200

    #38
    Changes in the comments

--- backend/middleware/auth.go
+++ backend/middleware/auth.go
@@ -14,52 +14,52 @@ func AuthMiddleware(c *gin.Context) {
 	authHeader := c.GetHeader("Authorization")
 
 	if authHeader == "" {
-		c.JSON(401, gin.H{"error": "No autorizado"})
+		c.JSON(401, gin.H{"error": "Not Authorized"})
 		c.Abort()
 		return
 	}
 
 	tokenString := strings.TrimPrefix(authHeader, "Bearer ")
 
-	// Leemos el secreto de las variables de entorno de tu ordenador o servidor
+	// We read the secret of environment variables
 	secretKey := os.Getenv("SUPABASE_JWT_SECRET")
 	if secretKey == "" {
-		// Si se nos olvidó configurarlo, paramos el servidor por seguridad
-		c.JSON(500, gin.H{"error": "Error de configuración del servidor"})
+		// If not configured, the server is shut down for security
+		c.JSON(500, gin.H{"error": "Error of the server configuration"})
 		c.Abort()
 		return
 	}
 
-	// 1. Analizamos y validamos el token usando el secreto de Supabase
+	// The token is analyzed and validated
 	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
 		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-			return nil, fmt.Errorf("método de firma inesperado")
+			return nil, fmt.Errorf("Unexpected signing method")
 		}
-		// Convertimos el string a []byte que es lo que pide la librería
+		// Converts the string to []byte
 		return []byte(secretKey), nil
 	})
 
 	if err != nil || !token.Valid {
-		c.JSON(http.StatusUnauthorized, gin.H{"error": "Token inválido o expirado"})
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid token or expired"})
 		c.Abort()
 		return
 	}
 
-	// 2. Extraemos los datos (claims) que Supabase metió dentro del token
+	// The data inside the token is extracted
 	if claims, ok := token.Claims.(jwt.MapClaims); ok {
-		// En Supabase, el ID del usuario SIEMPRE viene en el campo "sub"
+		// The user ID comes in the "sub" field
 		userID, idExists := claims["sub"].(string)
 
 		if !idExists {
-			c.JSON(http.StatusUnauthorized, gin.H{"error": "El token no contiene un ID de usuario"})
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "The token doesn't contain a user ID"})
 			c.Abort()
 			return
 		}
 
-		// 3. Guardamos el UUID que nos dio Supabase en el contexto de Gin
+		// Is saved the UUID in the context of Gin
 		c.Set("userID", userID)
 	} else {
-		c.JSON(http.StatusUnauthorized, gin.H{"error": "Error al leer el token"})
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Error reading the token"})
 		c.Abort()
 		return
 	}
