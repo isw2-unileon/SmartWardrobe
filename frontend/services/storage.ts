8013a075d7c4a34914f4a2018a2c96a36398530a
Ana <azarzu00@estudiantes.unileon.es>
2026-06-02 14:07:30 +0200

    storage.ts created

---
+++ frontend/services/storage.ts
@@ -0,0 +1,36 @@
+"use server";
+
+import { createClient } from "@/utils/supabase/server";
+import { cookies } from "next/headers";
+
+export async function uploadImage(
+  formData: FormData
+) {
+  const file = formData.get(
+    "file"
+  ) as File;
+
+  const cookieStore = await cookies();
+  const supabase = createClient(cookieStore);
+
+  const fileName = `${Date.now()}-${file.name}`;
+
+  const { data, error } =
+    await supabase.storage
+      .from("wardrobe-images")
+      .upload(fileName, file);
+
+  if (error) {
+    throw new Error(error.message);
+  }
+
+  const {
+    data: { publicUrl },
+  } = supabase.storage
+    .from("wardrobe-images")
+    .getPublicUrl(fileName);
+
+  console.log(publicUrl);
+
+  return publicUrl;
+}
