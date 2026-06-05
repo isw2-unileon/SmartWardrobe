9d1924cfa942f58cba9f43cea615ea9ee616dde5
InésCifuentes <inescifuerrai@gmail.com>
2026-06-05 05:57:15 +0200

    added routes

--- backend/internal/routes/routes.go
+++ backend/internal/routes/routes.go
@@ -4,14 +4,14 @@ import (
 	"backend/internal/handlers"
 	"backend/internal/repository"
 	"backend/internal/services"
+	"os"
 
 	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
 )
 
 // SetupRoutes set up all endpoints
-// func SetupRoutes(r *gin.Engine, db *gorm.DB, clipSvc *services.ClipService) {
-func SetupRoutes(r *gin.Engine, db *gorm.DB) {
+func SetupRoutes(r *gin.Engine, db *gorm.DB, clipSvc *services.ClipService) {
 
 	// A. Instantiate the Repository
 	masterColorRepo := repository.NewMasterColorRepository(db)
@@ -29,14 +29,17 @@ func SetupRoutes(r *gin.Engine, db *gorm.DB) {
 	locationService := services.NewLocationService()
 	weatherService := services.NewWeatherService()
 	outfitService := services.NewOutfitService(locationService, weatherService, clothingItemService, masterCategoryService, masterTypeService)
+	removeBgService := services.NewRemoveBGService(os.Getenv("REMOVEBG_API_KEY"))
 
 	// C. Instantiate the Handler and inject the Service
 	masterColorHandler := handlers.NewMasterColorHandler(masterColorService)
 	masterTypeHandler := handlers.NewMasterTypeHandler(masterTypeService)
 	masterStyleHandler := handlers.NewMasterStyleHandler(masterStyleService)
-	//clipHandler := handlers.NewClipHandler(clipSvc)
 	clothingItemsHandler := handlers.NewClothingItemHandler(clothingItemService)
 	outfitHandler := handlers.NewOutfitHandler(outfitService)
+
+	removeBgHandler := handlers.NewBackgroundRemoverHandler(removeBgService)
+	clipHandler := handlers.NewClipHandler(clipSvc)
 
 	// We group routes under /api for clean URLs
 	api := r.Group("/api")
@@ -46,9 +49,6 @@ func SetupRoutes(r *gin.Engine, db *gorm.DB) {
 		api.GET("/getAllTypes", masterTypeHandler.GetAll)
 
 		api.GET("/getAllStyles", masterStyleHandler.GetAll)
-
-		// CLIP
-		//api.POST("/clothing/analyze", clipHandler.Analyze)
 
 		api.GET("/clothingItems", clothingItemsHandler.GetAll)
 
@@ -64,6 +64,9 @@ func SetupRoutes(r *gin.Engine, db *gorm.DB) {
 
 		api.POST("/generateOutfit/days", outfitHandler.GenerateOutfit)
 
+		api.POST("/removeBackground", removeBgHandler.RemoveBackground)
+
+		api.POST("/clothing/analyze", clipHandler.Analyze)
 	}
 
 }
