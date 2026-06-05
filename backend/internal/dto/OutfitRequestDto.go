d236cc093bc127eafe363608296904060a2c5f77
emorio00 <emorio00@estudiantes.unileon.es>
2026-06-05 15:42:19 +0200

    #45
    Need the city and country to generate an outfit

--- backend/internal/dto/OutfitRequestDto.go
+++ backend/internal/dto/OutfitRequestDto.go
@@ -2,6 +2,7 @@ package dto
 
 type OutfitRequestDto struct {
 	City      string `json:"city" binding:"required"`
+	Country   string `json:"country" binding:"required"`
 	StartDate string `json:"start_date" binding:"required"`
 	EndDate   string `json:"end_date" binding:"required"`
 }
